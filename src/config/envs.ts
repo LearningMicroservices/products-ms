7da98c75eef274418d42b30cd2cd97345254ad45
Juan José Medina <jjr20060326@gmail.com>
2025-02-02 21:47:13 -0500

    nats

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -4,13 +4,19 @@ import * as Joi from 'joi';
 interface EnvVars {
   PORT: number;
   DATABASE_URL: string;
+
+  NATS_SERVERS: string[];
 }
 const envSchema = Joi.object({
   PORT: Joi.number().required(),
   DATABASE_URL: Joi.string().required(),
+  NATS_SERVERS: Joi.array().items(Joi.string()).required(),
 }).unknown(true);
 
-const { error, value } = envSchema.validate(process.env);
+const { error, value } = envSchema.validate({
+  ...process.env,
+  NATS_SERVERS: process.env.NATS_SERVERS?.split(','),
+});
 
 if (error) {
   throw new Error(`Config validation error: ${error.message}`);
@@ -21,4 +27,5 @@ const envVars: EnvVars = value;
 export const envs = {
   PORT: envVars.PORT,
   DATABASE_URL: envVars.DATABASE_URL,
+  NATS_SERVERS: envVars.NATS_SERVERS,
 };
--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -10,34 +10,36 @@ export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   // @Post()
-  @MessagePattern({ cmd: 'createProduct' })
+  @MessagePattern('createProduct')
   create(@Payload() createProductDto: CreateProductDto) {
     return this.productsService.create(createProductDto);
   }
 
   //@Get()
-  @MessagePattern({ cmd: 'findAllProducts' })
+  @MessagePattern('findAllProducts')
   findAll(@Payload() paginationDto: PaginationDto) {
     return this.productsService.findAll(paginationDto);
   }
 
   // @Get(':id')
-  @MessagePattern({ cmd: 'findOneProduct' })
+  @MessagePattern('findOneProduct')
   findOne(@Payload('id') id: string) {
     if (isNaN(+id)) {
+      console.log(id);
+
       throw new RpcException('Invalid id');
     }
     return this.productsService.findOne(+id);
   }
 
   // @Patch(':id')
-  @MessagePattern({ cmd: 'updateProduct' })
+  @MessagePattern('updateProduct')
   update(@Payload() updateProductDto: UpdateProductDto) {
     return this.productsService.update(updateProductDto.id, updateProductDto);
   }
 
   // @Delete(':id')
-  @MessagePattern({ cmd: 'removeProduct' })
+  @MessagePattern('removeProduct')
   remove(@Payload('id') id: string) {
     if (isNaN(+id)) {
       throw new RpcException('Invalid id');
@@ -45,7 +47,7 @@ export class ProductsController {
     return this.productsService.remove(+id);
   }
 
-  @MessagePattern({ cmd: 'validateProducts' })
+  @MessagePattern('validateProducts')
   validateProduct(@Payload() ids: number[]) {
     return this.productsService.validateProducts(ids);
   }
